a6f1616d044d9c4c4396bfaa59c84a384c01d1dd
Ned Fenstermacher <naf12@psu.edu>
2019-12-17 20:28:23 -0500

    Update to use Laravel Passport.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,11 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <passport-clients></passport-clients>
+
+                    <passport-authorized-clients></passport-authorized-clients>
+
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
